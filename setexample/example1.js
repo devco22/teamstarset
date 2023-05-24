c1db944e95492569fdad71a760ad11a3b14eec95
unknownplayer27 <126305902+1proprogrammerchant@users.noreply.github.com>
2023-05-24 14:24:00 -0500

    Create example1.js

---
+++ setexample/example1.js
@@ -0,0 +1,9 @@
+let john = { name: "John" };
+
+// for every user, let's store their visits count
+let visitsCountMap = new Map();
+
+// john is the key for the map
+visitsCountMap.set(john, 123);
+
+alert( visitsCountMap.get(john) ); // 123
