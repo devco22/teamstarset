3d3567f8926f502b57ce0a573c9719fb9c42840f
unknownplayer27 <126305902+1proprogrammerchant@users.noreply.github.com>
2023-05-24 14:22:53 -0500

    Create mapset.py

---
+++ mapset.py
@@ -0,0 +1,12 @@
+let map = new Map();
+
+map.set('1', 'str1');   // a string key
+map.set(1, 'num1');     // a numeric key
+map.set(true, 'bool1'); // a boolean key
+
+// remember the regular Object? it would convert keys to string
+// Map keeps the type, so these two are different:
+alert( map.get(1)   ); // 'num1'
+alert( map.get('1') ); // 'str1'
+
+alert( map.size ); // 3
