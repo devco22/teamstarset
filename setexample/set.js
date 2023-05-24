782dbfc88e5e5a77e3f9103431897e9b77f6ce88
unknownplayer27 <126305902+1proprogrammerchant@users.noreply.github.com>
2023-05-24 14:28:43 -0500

    Create set.js

---
+++ setexample/set.js
@@ -0,0 +1,19 @@
+let set = new Set();
+
+let john = { name: "John" };
+let pete = { name: "Pete" };
+let mary = { name: "Mary" };
+
+// visits, some users come multiple times
+set.add(john);
+set.add(pete);
+set.add(mary);
+set.add(john);
+set.add(mary);
+
+// set keeps only unique values
+alert( set.size ); // 3
+
+for (let user of set) {
+  alert(user.name); // John (then Pete and Mary)
+}
